34f070af94a4526d4397c456989ef8c432238d1e
pickled cabbage fish                                          <491978944@qq.com>
2020-02-11 23:07:51 +0800

    剑指offer

--- 43_NumberOf1/src/main/java/com/xuzf/NumberOfOne.java
+++ 43_NumberOf1/src/main/java/com/xuzf/NumberOfOne.java
@@ -6,6 +6,73 @@ package com.xuzf;
  * Date: 2020/2/11
  * Time: 2:16 PM
  * Description: No Description
+ *从1到n整数中1出现的次数
+ *
+ * 输入一个整数n，求从1到n这n个整数的十进制表示中1出现的次数。
+ * 例如输入12，从1到12这些整数中包含1 的数字有1，10，11和12，1一共出现了5次。
  */
 public class NumberOfOne {
+
+
+    public static void main( String[] args ) {
+        System.out.println(numberOfOne1(20938));
+        System.out.println(numberOfOne2(20938));
+    }
+
+    /***
+     * 这种时间复杂度是O(logNum)
+     * @param num
+     * @return
+     */
+    public static int numberOfOne1(int num){
+        int count=0;
+        //从十位开始，十位、百位、千位、万位
+        for(int i=1;i<=num;i*=10){  //i代表位数,分别是10，100，1000，。。。
+            int high=num/(i*10); //更高位数字
+            int low=(num%i);  //更低位数字
+            int cur=(num/i)%10;  //当前位数字
+            count+= high*i;
+
+            if(cur==0){
+//                count+=high*i;
+            }else if(cur==1){
+//                count+=high*i+(low+1);
+                count+=low+1;
+            }else{
+//                count+=(high+1)*i;
+                count+=i;
+            }
+        }
+        return count;
+    }
+
+    /***
+     * 不断的循环遍历每个数，把这些数的1的个数相加求和
+     * @param n
+     * @return
+     */
+    public static int numberOfOne2(int n){
+        int count=0;
+        for(int i=1;i<=n;i++){  //i代表位数
+            count=count+count(i);
+        }
+        return count;
+    }
+
+    /***
+     * 每个数都判断有几个1
+     * @param i
+     * @return
+     */
+    public static int count(int i){
+        int c=0;
+        while(i>0){
+            if(i%10==1){
+               c++;
+            }
+            i=i/10;
+        }
+        return c;
+    }
+
 }
--- 44_DigitsInSequence/src/main/java/com/xuzf/DigitsInSequence.java
+++ 44_DigitsInSequence/src/main/java/com/xuzf/DigitsInSequence.java
@@ -6,6 +6,93 @@ package com.xuzf;
  * Date: 2020/2/11
  * Time: 2:17 PM
  * Description: No Description
+ * 数字序列中某一位的数字
+ *
+ * 数字以0123456789101112131415…的格式序列化到一个字符序列中。在这个序列中，第5位（从0开始计数）是5，第13位是1，第19位是4，等等。
+ * 请写一个函数求任意位对应的数字。
  */
 public class DigitsInSequence {
+
+    public static void main( String[] args ) {
+        System.out.println(findDigitsInSequence(5));
+//        System.out.println(findDigitsInSequence(11));
+//        System.out.println(findDigitsInSequence(12));
+//        System.out.println(findDigitsInSequence(13));
+//        System.out.println(findDigitsInSequence(14));
+        System.out.println(findDigitsInSequence(257));
+    }
+
+    public static Integer findDigitsInSequence(Integer number){
+        int current=0;
+        int count=0;
+        while (true){
+            current++;
+            int num = numbersOfIntegers(current);
+            //System.out.println(num);
+            if(count+num>number){
+                break;
+            }
+            count=count+num;
+        }
+        if(current==1){
+            return number-1;
+        }
+       return getDigit(current,number-count);
+    }
+
+    /***
+     * 获取数字
+     * @param subcount
+     * @param m
+     * @return
+     */
+    private static int getDigit( int m,int subcount) {
+        int beginIndex = getFirstNumber(m);
+        int number =-1;
+        //先找到那个值
+        int yushu = subcount%m;
+        int indexFromRight = 0;
+        if(yushu==0){
+            number =beginIndex+subcount/m-1;
+        }else{
+            number =beginIndex+subcount/m;
+            indexFromRight = m-subcount%m;
+        }
+
+        System.out.println(number);
+//        System.out.println(subcount%m);
+//        int indexFromRight = m-subcount%m;//在数字中的位置
+//        System.out.println(indexFromRight);
+        for(int i=0;i<indexFromRight;i++)
+            number/=10;
+        return number%10;
+    }
+
+    /*
+     * 第一个m位数
+     * 例如第一个两位数是10，第一个三位数是100
+     */
+    private static int getFirstNumber(int m) {
+        if(m==1)
+            return 0;
+        return (int) Math.pow(10, m-1);
+    }
+
+    /*
+     * 返回m位数的总个数
+     * 例如，两位数一共有90个：10~99；三位数有900个：100~999
+     */
+    public static Integer numbersOfIntegers(Integer m){
+        if(m==1){
+            return 10;
+        }
+        int result = -1;
+        int sub = 1;
+        int n = m;
+        while(n>1){
+            sub*=10;
+            n--;
+        }
+        return m*sub*9;
+    }
 }
