55b436cad2446c7ec8072ac6aaadb49ac9803292
pickled cabbage fish                                          <491978944@qq.com>
2020-02-22 15:03:42 +0800

    剑指offer

---
+++ 02_Structs/src/main/java/com/xuzf/BinaryHeap.java
@@ -0,0 +1,155 @@
+package com.xuzf;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhifang.xu
+ * Date: 2020/2/22
+ * Time: 2:06 PM
+ * Description: No Description
+ * 二叉堆（二叉堆是一个完全二叉树）
+ */
+public class BinaryHeap {
+    static Integer[] heap =new Integer[16];
+    /***
+     * 当前二叉堆的大小
+     */
+    static int size=0;
+
+    public static void main( String[] args ) {
+
+        add(10);
+        add(5);
+        add(3);
+        add(7);
+        add(6);
+        add(8);
+        add(1);
+
+        remove();
+        remove();
+        remove();
+        remove();
+        remove();
+        remove();
+        remove();
+    }
+
+    /***
+     * left = parentIndex*2+1;
+     * right = parentIndex*2+2;
+     * @param node
+     */
+    public static void add(Integer node){
+        if(size==0){
+            heap[size++]=node;
+            return ;
+        }
+        if(size==heap.length){
+            Integer[] newheap =new Integer[heap.length*2];
+            System.arraycopy(heap,0,newheap,0,heap.length);
+            heap=heap;
+        }
+        //先把元素加到数组里，然后进行上浮
+        heap[size++]=node;
+        Integer currentIndex =size-1;
+        /***
+         * 不断的上浮
+         */
+        adjustNode(heap,currentIndex);
+        System.out.println("上浮结束");
+        System.out.println(heap[0]);
+    }
+
+    public static void remove(){
+        if(size==0){
+            throw new ArrayIndexOutOfBoundsException("堆是空的");
+        }
+        if(size==1){
+            heap[0]=null;
+            size--;
+            return ;
+        }
+        //把完全二叉树的最后一个元素放到头节点上
+        System.out.println(heap[0]);
+        heap[0]=heap[size-1];
+        heap[size-1]=null;
+        size--;
+        downNode(heap,0);
+    }
+
+    /**
+     * 上浮
+     * @param heap
+     * @param index 当前节点的索引
+     * @return
+     */
+    public static void adjustNode(Integer[] heap,Integer index){
+        if(index==0){//如果是根节点，就无需下沉
+            return;
+        }
+        int parentIndex=0;
+        while(true){
+            if(index==0){
+                break;
+            }
+            if(index%2==1){//表示当前左子节点
+                parentIndex=(index-1)/2;
+            }else{//表示当前右子节点
+                parentIndex=(index-2)/2;
+            }
+            //如果子节点比父节点大，则停止上浮了
+            if(heap[index]>=heap[parentIndex]){
+                break;
+            }
+            /***
+             * 如果当前节点比父节点小的话，上浮
+             */
+            int temp = heap[index];
+            heap[index]=heap[parentIndex];
+            heap[parentIndex]=temp;
+            index=parentIndex;
+        }
+
+    }
+
+    /***
+     * 是否下沉成功
+     * @param heap
+     * @param index 父节点的索引
+     * @return
+     */
+    public static void downNode(Integer[] heap,int index){
+        //假如存在左右子节点，则获得两个左右子节点的索引
+        while(true){
+            int leftIndex = index*2+1;
+            int rightIndex = index*2+2;
+            //如果没有子节点了，则下沉结束
+            if(rightIndex>=size&&leftIndex>=size){
+                break;
+            }
+            //存放两个子节点中值较小的那个
+            int minChildIndex = 0;
+            if(rightIndex>=size){
+                minChildIndex=leftIndex;
+            }else if(leftIndex>=size){
+                minChildIndex=rightIndex;
+            }else{
+                minChildIndex=heap[leftIndex]>heap[rightIndex]?rightIndex:leftIndex;
+            }
+            //下载结束
+            if(heap[index]<heap[minChildIndex]){
+                break;
+            }
+            //如果父节点的值比子节点大，则下沉成功
+            if(heap[index]>heap[minChildIndex]){
+                int temp = heap[index];
+                heap[index]=heap[minChildIndex];
+                heap[minChildIndex]=temp;
+                index=minChildIndex;
+            }
+        }
+    }
+
+}
--- 02_Structs/src/main/java/com/xuzf/Tree.java
+++ 02_Structs/src/main/java/com/xuzf/Tree.java
@@ -2,6 +2,7 @@ package com.xuzf;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.concurrent.LinkedBlockingDeque;
 
 /**
@@ -23,6 +24,46 @@ public class Tree {
      * 用于做广度遍历
      */
     static LinkedBlockingDeque<BinaryTreeNode> queue=new LinkedBlockingDeque <>();
+
+    /***
+     *
+     *         10
+     *    5           12
+     * 4     7           13
+     * @param args
+     */
+    public static void main( String[] args ) {
+        BinaryTreeNode root = new BinaryTreeNode(10);
+        BinaryTreeNode node2 = new BinaryTreeNode(5);
+        BinaryTreeNode node3 = new BinaryTreeNode(12);
+        BinaryTreeNode node4 = new BinaryTreeNode(4);
+        BinaryTreeNode node5 = new BinaryTreeNode(7);
+        BinaryTreeNode node6 = new BinaryTreeNode(13);
+
+        root.left = node2;
+        root.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+//        node3.right=node6;
+        preOrderErgodic(root);
+        System.out.println();
+        preOrderErgodicWithFull(root,1,3);
+        System.out.println();
+        preOrderErgodicWithStack(root);
+        System.out.println();
+        inOrderErgodic(root);
+        System.out.println();
+        inOrderErgodicWithFull(root,1,3);
+        System.out.println();
+        postOrderErgodic(root);
+        System.out.println();
+        postOrderErgodicWithFull(root,1,3);
+
+
+    }
+
+
+
     /***
      * 前序遍历
      * 父节点-->左子树-->右子树
@@ -43,6 +84,29 @@ public class Tree {
         }
     }
 
+    /***
+     * 前序遍历-利用栈
+     * 父节点-->左子树-->右子树
+     */
+    public static void preOrderErgodicWithStack(BinaryTreeNode node){
+        Stack<BinaryTreeNode> s= new Stack <>();
+        /***
+         * 判断当前节点不为空，如果当前节点为空，则从栈中弹出父节点，去查看兄弟节点(父节点的右节点)
+         */
+        while(node!=null||!s.isEmpty()){
+            if(node!=null){//如果当前节点不为空，则继续判断当前节点的左节点
+                System.out.print(node.data+"-->");
+                s.add(node);
+                node=node.left;
+            }else {//如果当前节点为空，则从栈中弹出当前节点的父节点
+                node=s.pop();//从栈中弹出当前节点的父节点
+                node=node.right;//获得右节点
+            }
+
+
+        }
+    }
+
     /**
      * 当作一个满二叉树来遍历--前序遍历
      * 父节点-->左子树-->右子树
@@ -58,13 +122,13 @@ public class Tree {
         System.out.print(node.data+"-->");
         BinaryTreeNode left = node.left;
         if(left!=null){
-            preOrderErgodicWithFull(left,++currentLevel,depth);
+            preOrderErgodicWithFull(left,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
         BinaryTreeNode right = node.right;
         if(right!=null){
-            preOrderErgodicWithFull(right,++currentLevel,depth);
+            preOrderErgodicWithFull(right,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
@@ -103,7 +167,7 @@ public class Tree {
 
         BinaryTreeNode left = node.left;
         if(left!=null){
-            inOrderErgodicWithFull(left,++currentLevel,depth);
+            inOrderErgodicWithFull(left,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
@@ -112,7 +176,7 @@ public class Tree {
 
         BinaryTreeNode right = node.right;
         if(right!=null){
-            inOrderErgodicWithFull(right,++currentLevel,depth);
+            inOrderErgodicWithFull(right,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
@@ -151,12 +215,12 @@ public class Tree {
         }
 
         if(node.left!=null){
-            postOrderErgodicWithFull(node.left,++currentLevel,depth);
+            postOrderErgodicWithFull(node.left,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
         if(node.right!=null){
-            postOrderErgodicWithFull(node.right,++currentLevel,depth);
+            postOrderErgodicWithFull(node.right,currentLevel+1,depth);
         }else if(currentLevel<depth){
             result2.add(BinaryTreeNode.defaultTreeNode());
         }
